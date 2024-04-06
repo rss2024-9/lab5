ae53c8d8ec493a56d8584a70a01043d09cc76732
kramerkraus <mrkraus@mit.edu>
2024-04-06 20:39:09 +0000

    got rid of prints in part filt

--- localization/particle_filter.py
+++ localization/particle_filter.py
@@ -101,7 +101,7 @@ class ParticleFilter(Node):
         """
         with self.lock:
             probabilities = self.sensor_model.evaluate(self.particles, np.array(scan.ranges))
-            self.get_logger().info(str(np.sum(probabilities)))
+            #self.get_logger().info(str(np.sum(probabilities)))
             probabilities = probabilities/np.sum(probabilities)
             idx = np.random.choice(self.num_particles, self.num_particles, p=probabilities)
             self.particles = self.particles[idx, :]
